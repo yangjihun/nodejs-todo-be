071f1b4acfd922152f9b29b581eb0b9b18024b9b
jihun <yjhn0410@gmail.com>
2025-07-24 14:20:15 +0900

    init project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+const app = express();
+app.use(bodyParser.json());
+app.use('/api',indexRouter);
+const mongoURI = 'mongodb://localhost:27017/todo-demo';
+
+mongoose
+    .connect(mongoURI, {useNewUrlParser:true})
+    .then(() => {
+        console.log('mongoose connected')
+    })
+    .catch((err) => {
+        console.log('DB connection fail', err)
+    });
+
+app.listen(5000, () => {
+    console.log("server on 5000")
+});
+
