f04035a0c47cee1e93ed3cc1093f10fe8e4d0052
jihun <yjhn0410@gmail.com>
2025-07-24 14:26:11 +0900

    Initial commit

---
+++ controller/task.controller.js
@@ -0,0 +1,49 @@
+const Task = require("../model/Task");
+
+const taskController = {};
+
+taskController.createTask = async (req,res) => {
+    try{
+        const {task,isComplete} = req.body;
+        const newTask = new Task({task,isComplete});
+        await newTask.save();
+        res.status(200).json({status:'ok',data:newTask});
+    } catch (err) {
+        res.status(400).json({status:'fail',error:err});
+    }
+};
+
+taskController.getTask = async(req,res) => {
+    try{
+        const taskList = await Task.find({}).select("-__v")
+        res.status(200).json({status:'ok', data:taskList});
+    } catch (err) {
+        res.status(400).json({status:'fail', error: err});
+    }
+};
+
+taskController.updateTask = async(req,res) => {
+    try{
+        const task = await Task.findById(req.params.id);
+        console.log('find id');
+        const fields = Object.keys(req.body);
+        fields.map((item) => (task[item] = req.body[item]));
+        console.log(task);
+        await task.save();
+        res.status(200).json({status:'ok',data:task});
+    } catch (err) {
+        res.status(400).json({status:'fail', error:err});
+    }
+}
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const deleteItem = await Task.findByIdAndDelete(req.params.id);
+    console.log('find id and delete')
+    res.status(200).json({ status: "success", data: deleteItem });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports = taskController;
