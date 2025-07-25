8d7f73e4f7aef35059f9512499172884ebaf20df
jihun <yjhn0410@gmail.com>
2025-07-25 18:39:09 +0900

    port수정

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ mongoose
         console.log('DB connection fail', err)
     });
 
-app.listen(5000, () => {
-    console.log("server on 5000")
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("server on")
 });
 
